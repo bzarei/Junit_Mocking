a0b294c96f70b3fd318d59650484d069ad8b166b
Baharak Zarei <baharak.zarei@telekom.de>
2021-08-26 14:02:20 +0200

    added test class for App

--- src/main/java/de/telekom/sea4/mockito/App.java
+++ src/main/java/de/telekom/sea4/mockito/App.java
@@ -2,10 +2,28 @@ package de.telekom.sea4.mockito;
 
 public class App {
 
+	private Top top;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        
+		Bottom bottom = new Bottom();
+		Middle middle = new Middle(bottom);	
+		Top top = new Top(middle);
+
+		App app = new App(top);
+		
+		app.run(args); 
 		System.out.println("Hello SEA Kollegen");
 
 	}
-
+	
+	public void run(String[] args) {		
+		
+		System.out.println(top.topMethode());
+	
+	}
+	
+	public App(Top top) {
+		this.top = top;
+	}
 }
